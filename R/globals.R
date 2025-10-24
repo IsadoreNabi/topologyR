7cb814f54c9d0536339061f908757f8b3cdb2659
José Mauricio Gómez Julián <isadore.nabi@pm.me>
2025-10-23 21:15:27 -0600

    Update to version 0.1.1

--- R/globals.R
+++
@@ -1,9 +0,0 @@
-#' Package internal globals
-#' @keywords internal
-#' @noRd
-#' @importFrom utils globalVariables
-NULL
-
-globalVariables(c(
-  "base_size", "max_set_size", "min_set_size", "method", "threshold", "iqr_factor"
-))
